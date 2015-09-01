5182df029c25b0d1414d58d819a3cf8501903cc3
jessicalc <jessicalaurenchong@gmail.com>
2015-08-31 19:04:52 -0700

    sanitized string input from users, no XSS please

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,7 +19,7 @@ app.init = function() {
   app.fetch = fetch;
   app.server = "https://api.parse.com/1/classes/chatterbox";
   app.clearMessages = clearMessages;
-  app.addMessage = addMessage;
+  app.addMessageToUI = addMessageToUI;
   app.addRoom = addRoom;
   app.addFriend = addFriend;
   app.handleSubmit = handleSubmit;
@@ -29,13 +29,19 @@ var fetch = function() {
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    dataType: 'jsonp',
-    contentType: 'application/jsonp',
+    contentType: 'application/json',
     success: function(data) {
-      console.log(data);
+      var messages = data.results;
+      console.log(messages);
+      var temp = {};
+      for (var i = 0; i < messages.length; i++) {
+        temp.username = sanitizeString(messages[i]['username']);
+        temp.text = sanitizeString(messages[i]['text']);
+        addMessageToUI(temp);
+      }
     },
     error: function(data) {
-      console.log(data);  
+      console.log(data);
     }
   });
 }
@@ -55,12 +61,33 @@ var send = function(message) {
   });
 }
 
+var sanitizeString = function(string) {
+  return html_sanitize(string);
+}
+
+var displayPosts = function(data) {
+  console.log(data);
+}
+
 var clearMessages = function() {
   $('#chats').empty();
 }
 
 // validate our strings against XSS attacks
-var addMessage = function(text) {
+var addMessageToUI = function(message) {
+  var $node = '<div><span class="username">' + message.username + '</span><span class="text">' + message.text + '</span></div>';
+  $('#chats').append($node);
+}
+
+var addRoom = function(roomName) {
+  $('#roomSelect').append('<option value ="' + sanitizeString(roomName) + '"></option>');
+}
+
+var addFriend = function(friend) {
+  console.log('yay!');
+}
+
+var getUsername = function() {
   var userNameParam = window.location.search;
   var startIdx = userNameParam.indexOf('username') + 9;
 
@@ -71,30 +98,17 @@ var addMessage = function(text) {
 
     username += userNameParam[i];
   }
+  return username;
+}
 
+var handleSubmit = function() {
   var message = {};
-  message.username = username;
-  message.text = text;
 
-  var $node = '<div><span class="username">' + message.username + '</span><span class="text">' + message.text + '</span></div>';
-  $('#chats').append($node);
+  message.text = sanitizeString($('#message').val());
+  message.username = getUsername();
+  app.addMessageToUI(message);
 
   app.send(message);
 }
 
-var addRoom = function(roomName) {
-  $('#roomSelect').append('<option value ="' + roomName + '"></option>');
-}
-
-var addFriend = function(friend) {
-  console.log('yay!');
-}
-
-var handleSubmit = function() {
-  var message = $('#message').val();
-  app.addMessage(message);
-}
-
-// file:///Users/student/Documents/chatterbox-client/client/index.html?message=dff&submit=Submit&username=anonymous# 
-
-//slice it from _.indexOf(url, "username") to 9
+setInterval(app.fetch, 1000);
